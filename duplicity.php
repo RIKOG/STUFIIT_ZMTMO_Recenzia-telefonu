96eaa30c30c1e7f7305f1016a764c1a131c5f1b8
Richard Gajdosik <rich.gajdosik@gmail.com>
2019-12-08 15:20:14 +0100

    Dokoncene dynamicke nacitavanie

--- duplicity.php
+++ duplicity.php
@@ -1,5 +1,7 @@
 <?php include 'init.php'; ?>
 <?php include $_SESSION['lang'] == 'sk' ? 'sk.php' : 'en.php'; ?>
+<?php include 'poleobrazkov.php'; ?>
+
 <html>
  <head>
    <title>iPhone</title>
@@ -8,10 +10,4 @@
      <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
  </head>
  <body background="/images/pozadie.jpg">
-  <?php include 'header_inc.php'; ?>
-    <div class "container2">
-  <h1><center>Richard Gajdošík - iPhone 6s 64GB Silver</center></h1>
-  <?= $_SESSION['lang'] == 'sk' ? 'SLOVENSKO' : 'ANGLICKO' ?>
-  </div>
-<img src="/images/KOMBO.jpg" class="container2">
-</body>
+ <?php include 'header_inc.php'; ?>
